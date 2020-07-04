65ae1e39edef1523781f2cddf3f1b2e4c4ff81ef
Aditya Patwal <adityapatwal.152@gmail.com>
2020-07-04 23:06:15 +0530

    Updates

---
+++ precache-manifest.e875fd4c8d43783a33c1477de6d33e8a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e48c53da22c3a3dcfa8d9b16ec2f66b",
+    "url": "/Clothing/index.html"
+  },
+  {
+    "revision": "8e10b7bd6d3a23f5f7fc",
+    "url": "/Clothing/static/css/main.025c7080.chunk.css"
+  },
+  {
+    "revision": "d737f0e8f0226ab6ef71",
+    "url": "/Clothing/static/js/2.8c06b0a8.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/Clothing/static/js/2.8c06b0a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e10b7bd6d3a23f5f7fc",
+    "url": "/Clothing/static/js/main.389305a1.chunk.js"
+  },
+  {
+    "revision": "42b2e2f64673eac10518",
+    "url": "/Clothing/static/js/runtime-main.812a5a0e.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/Clothing/static/media/crown.19ef7b17.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/Clothing/static/media/shopping-bag.30f07071.svg"
+  }
+]);
